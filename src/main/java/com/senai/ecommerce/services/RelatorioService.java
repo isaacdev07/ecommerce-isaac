420a4940dca24ce2fa5ad15bca411181f21a7d29
isaacdev07 <isaacsantos7953@gmail.com>
2025-05-15 09:48:02 -0300

    update e relatorios

---
+++ src/main/java/com/senai/ecommerce/services/RelatorioService.java
@@ -0,0 +1,48 @@
+package com.senai.ecommerce.services;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.senai.ecommerce.dto.RelatorioPedidoDTO;
+import com.senai.ecommerce.entities.Pedido;
+import com.senai.ecommerce.repositories.PedidoRepository;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
+@Service
+public class RelatorioService {
+	
+	@Autowired
+	PedidoRepository pedidoRepository;
+	
+	public void gerarRelatorio(String caminho) throws JRException {
+		List<Pedido> pedidos = pedidoRepository.findAll();
+
+		List<RelatorioPedidoDTO> pedidosRelatorio = pedidos.stream().map(RelatorioPedidoDTO::new)
+				.collect(Collectors.toList());
+
+		JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(pedidosRelatorio);
+
+		Map<String, Object> parametros = new HashMap<>();
+		parametros.put("titulo", "Relatórios de Pedidos");
+
+		JasperReport jasperReport = JasperCompileManager
+				.compileReport(getClass().getResourceAsStream("/relatorios/relatorio_pedidos.jrxml"));
+
+		JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parametros, dataSource);
+
+		JasperExportManager.exportReportToPdfFile(jasperPrint, caminho);
+
+	}
+	}
