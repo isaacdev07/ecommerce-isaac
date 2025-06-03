fcba8b9b4608cc8e82f39ed24ef2e114d6dabc74
isaacdev07 <isaacsantos7953@gmail.com>
2025-06-03 16:04:20 -0300

    Atualização nao finalizada

---
+++ src/main/java/com/senai/ecommerce/config/SecurityFilter.java
@@ -0,0 +1,47 @@
+package com.senai.ecommerce.config;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.senai.ecommerce.repositories.UsuarioRepository;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    TokenService tokenService;
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var Token = this.recoverToken(request);
+        if (Token != null){
+            var login = tokenService.ValidatToken(Token);
+            UserDetails user = usuarioRepository.findByLogin(login);
+
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+}
--- src/main/java/com/senai/ecommerce/repositories/UsuarioRepository.java
+++ src/main/java/com/senai/ecommerce/repositories/UsuarioRepository.java
@@ -1,10 +1,12 @@
 package com.senai.ecommerce.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import com.senai.ecommerce.entities.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	Usuario findByEmail(String email);
+	UserDetails findByLogin(String email);
 }
